2cb10d5478f8476a07c3a9b899d2a2e36f671d40
Jackhard <jackhard.z@gmail.com>
2022-12-30 13:34:44 -0300

    todo
    All done!

--- src/components/currency — рабочая.js
+++ src/components/currency — рабочая.js
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-import './currency.css';
 
 const Currency = () => {
 	const { currency,dispatch,expenses } = useContext(AppContext);
@@ -29,7 +28,7 @@ const Currency = () => {
                     <div className="input-group-prepend">
                 <label  htmlFor="inputGroupSelect01" >Currency</label>
                 				
-                  <select className="custom-select" id="inputGroupSelect01"  onChange={(event) => changeCurrency(event.target.value)}>
+                  <select className="custom-select" id="inputGroupSelect01" input-color="Green" onChange={(event) => changeCurrency(event.target.value)}>
                        <option defaultValue>Select currency...</option>
                         <option value="$" name="dollar">$ Dollar</option>
 						<option value='£' name="pounds">£ Pounds</option>
@@ -44,9 +43,3 @@ const Currency = () => {
 
 export default Currency;
 
-/*
-<select class="select" name="colors" onchange="this.style.backgroundColor=this.options[this.selectedIndex].style.backgroundColor"> 
-    <option style="background-color:red" value="red">Red</option> 
-    <option style="background-color:green" value="green">Green</option>
-</select>
-*/
